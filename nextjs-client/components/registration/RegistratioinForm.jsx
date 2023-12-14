b428027d0b5ded4e6c731de806c69eea4461e83e
Paul Bodner <bodner80@gmail.com>
2023-12-14 14:44:34 -0800

    implemented ResgistrationForm Validation for matching passwords. Added Link to Header logo back to home page

--- nextjs-client/components/registration/RegistratioinForm.jsx
+++ nextjs-client/components/registration/RegistratioinForm.jsx
@@ -4,11 +4,10 @@ import { useState, useRef } from "react";
 export default function RegistrationForm({ styles, handleSubmit,  formRef, errObj}){
 
   const validate = (formData, setErrObj) => {
-    error={}
+    const error={}
     if (formData.get('password') !== formData.get('confirm_password')){
       error.matchingPasswords = 'The two passwords provided do not match.'
     }
-
     setErrObj(prev=>({...error}));
   }
 
@@ -20,34 +19,50 @@ export default function RegistrationForm({ styles, handleSubmit,  formRef, errOb
       className="w-full flex flex-col"
     >
       <div className="w-full flex flex-col sm:flex-row">
-        <div className="w-full flex flex-col sm:pe-1 my-2">
+        <div className="w-full sm:w-1/2 flex flex-col sm:pe-1 my-2">
           <label className='mb-1' htmlFor="first_name">First Name:</label>
-          <input className="rounded-md" type='text' name='first_name' required/>
+          <input className="rounded-md p-0.5" type='text' name='first_name' required/>
         </div>
-        <div className="w-full flex flex-col sm:ps-1 my-2">
+
+        <div className="w-full sm:w-1/2 flex flex-col sm:ps-1 my-2">
           <label className='mb-1' htmlFor="Last_name">Last Name:</label>
-          <input className="rounded-md" type='text' name='Last_name' required/>
+          <input className="rounded-md p-0.5" type='text' name='Last_name' required/>
         </div>
       </div>
 
       <div className="w-full flex flex-col my-2">
         <label className='mb-1' htmlFor="email">Email:</label>
-        <input className="rounded-md" type='email' name='email' value='bodner80@gmail.com' required/>
+        <input className="rounded-md p-0.5" type='email' name='email' required/>
       </div>
-      <div className="w-full flex flex-col my-2">
+
+      <div className="relative w-full flex flex-col my-2 mt-4">
+        { errObj.matchingPasswords && <p className="absolute -translate-y-3/4 top-0 text-sm text-red-500">*{ errObj.matchingPasswords }</p> }
         <label className='mb-1' htmlFor='password'>Password:</label>
-        <input className="rounded-md" type='password' name='password' required/> 
+        <input 
+          className={["rounded-md p-0.5", (errObj.matchingPasswords ? 'border-4 border-red-500' : '')].join(' ')} 
+          type='password' 
+          name='password' 
+          required
+        /> 
       </div>
+
       <div className="w-full flex flex-col my-2">
         <label className='mb-1' htmlFor='confirm-password'>Confirm Password:</label>
-        <input className="rounded-md" type='password' name='confirm_password' required/> 
+        <input 
+          className={["rounded-md p-0.5", (errObj.matchingPasswords ? 'border-4 border-red-500' : '')].join(' ')} 
+          type='password' 
+          name='confirm_password' 
+          required
+        /> 
       </div>
+
       <div className="w-full my-2">
         <input className="me-2 h-4 w-4" type='checkbox' name='notifications'/>
         <label className='text-base' htmlFor='notifications'>I would like to recieve updates and notifications from Regeneration Room</label>
       </div>
+
       <div>
-        <input className='border-2 border-secondary-action text-white rounded-lg px-4 py-1' type='submit' value='Register'/>
+        <input className='border-2 border-secondary-action text-white rounded-lg px-4 py-1 hover:cursor-pointer' type='submit' value='Register'/>
       </div>
     </form>
   );
