67e4246e2f2533195767986002f038d2a817f52d
Paul Bodner <50117793+mrman511@users.noreply.github.com>
2024-01-11 17:42:46 -0800

    Revert "Revert "added password reset page and form. changed Login and Registration Fo…""

---
+++ nextjs-client/components/login/password_reset/PasswordResetRequestForm.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const PasswordResetRequestForm = React.forwardRef(( {handleSubmit, transition, confirmationObj}, ref )=>{
+
+  function responseFunction(){
+    const confirmation = { link: { path: '/login', text: 'Login'  } , message: 'Check if your email for a link to reset your password', error: false, title: 'Sucess!' };
+    confirmationObj.current=confirmation;
+    transition('CONFIRM')
+  };
+  
+  const submissionObj = {
+    responseFunction: responseFunction,
+    catchFunction: responseFunction
+  };
+
+  return(
+    <>
+      <form 
+        method='POST' 
+        ref={ ref } 
+        onSubmit={ (e) => { handleSubmit(e, '/users/reset_password', submissionObj) } } 
+        className="w-full flex flex-col"
+      >
+        <div className="w-full flex flex-col mb-4">
+          <label className='mb-1' htmlFor="Email">Email:</label>
+          <input className="p-1 rounded-md" type='text' name='email' required/>
+        </div>
+        <div>
+          <input className='border-2 border-secondary-action rounded-lg px-4 py-1 text-white hover:cursor-pointer hover:text-secondary-action' type='submit' value='Send Request'/>
+        </div>
+      </form>
+    </>
+  );
+})
+
+export default PasswordResetRequestForm
