655d005e39d1ea99bb60c08a137363b0e6505129
Paul Bodner <bodner80@gmail.com>
2024-03-05 15:17:22 -0800

    added get user data on login

---
+++ nextjs-client/app/admin/page.js
@@ -0,0 +1,14 @@
+'use client'
+import { useCookies } from "react-cookie";
+import Header from "@/components/Header";
+
+export default function Login() {
+  const [cookie, setCookie, removeCookie] = useCookies(['user'])
+
+  return (
+    <main className="flex min-h-screen flex-col items-center font-base">
+      <Header styles={ styles }/>
+      
+    </main>
+  )
+}
--- nextjs-client/utils/api-requests/apiRequests.js
+++ nextjs-client/utils/api-requests/apiRequests.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 export default function apiRequest(requestObject, responseFunction, catchFunction){
-  const { path, method, body } = requestObject
+  const { path, method, body, headers } = requestObject
   const request = [process.env.NEXT_PUBLIC_API_BASE_ROUTE + path]
   if ( body ){
     request.push(body)
   }
-  axios[method](...request).then((res)=>{
+  const options = {}
+  if (headers){
+    options.headers={...headers}
+  }
+  axios[method](...request, options).then((res)=>{
     responseFunction(res);
   }).catch((err)=>{
     if (catchFunction){
